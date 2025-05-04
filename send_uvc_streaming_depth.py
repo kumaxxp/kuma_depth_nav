411a7b8fcbc15e3c3816b52cac607a4829bef28f
kumaxxp <kuma_diy@hotmail.com>
2025-05-04 15:04:31 +0900

    深度マップの可視化処理を修正：元画像と深度特徴を横に並べるように変更

--- send_uvc_streaming_depth.py
+++ send_uvc_streaming_depth.py
@@ -111,7 +111,8 @@ def create_depth_visualization_ori(depth_map: np.ndarray, original_frame: np.nda
     depth_resized = cv2.resize(depth_colored, (original_frame.shape[1], original_frame.shape[0]))
     
     # 元画像と深度マップを横に並べる
-    return np.concatenate([original_frame, depth_resized], axis=1)
+#    return np.concatenate([original_frame, depth_resized], axis=1)
+    return np.concatenate([original_frame, depth_feature], axis=1)
 
 
 def create_depth_visualization(depth_map: np.ndarray, original_frame: np.ndarray) -> np.ndarray:
